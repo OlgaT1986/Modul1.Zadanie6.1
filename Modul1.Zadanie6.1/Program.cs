be55f125a5642de8993257a99e4762f1e1e4e370
Admin <Admin@ADMIN->
2021-12-24 22:58:11 +0700

    Добавьте файлы проекта.

---
+++ Modul1.Zadanie6.1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul1.Zadanie6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string words, lon;
+            Console.WriteLine("Введите предложение без знаков препинания");
+            words = Console.ReadLine();
+            string[] split = words.Split(' ');
+            lon = split[0];
+            foreach (string s in split)
+            {
+                if (s.Trim() != "")
+                {
+                    if (lon.Length < s.Length) lon = s;
+                }
+            }
+            Console.WriteLine("Самое длинное слово - " + lon);
+            Console.ReadKey();
+
+        }
+    }
+}
